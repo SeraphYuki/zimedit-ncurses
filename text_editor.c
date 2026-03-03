10439f5c6a5ffd42c7d2f17112a35d7d0ffcee3e
Blake Williamson <blake.williamson.cat@gmail.com>
2026-03-02 19:54:08 -0800

    fix same bug from last commit again

--- text_editor.c
+++ text_editor.c
@@ -9,17 +9,20 @@
 #include <string.h>
 #ifndef SDL_COMPILE
 #endif
+
 #ifdef SDL_COMPILE
 #ifdef SDL2_COMPILE
 #include <SDL2/SDL.h>
 #else
 #include <SDL3/SDL.h>
 #endif
+
 #ifdef LINUX_COMPILE
+#ifndef SDL_COMPILE
 #include "x11.h"
 #include <termios.h>
 #include <ncurses.h>
-
+#endif
 #ifdef __OpenBSD__
 #include <util.h>
 #else
@@ -1534,9 +1537,7 @@ static void Paste(Thoth_Editor *t, Thoth_EditorCmd *c){
 	for(k = 0; k < clipboardLen; k++){
 		if(clipboard[k] == '\n') lines++;
 	}
-#ifdef SDL_COMPILE
 	lines--;
-#endif
 
 	if(lines == t->nCursors){
 		
@@ -1561,8 +1562,15 @@ static void Paste(Thoth_Editor *t, Thoth_EditorCmd *c){
 
 	} else {
 		for(k = 0; k < t->nCursors; k++){
+			char tmp = clipboard[clipboardLen-1];
+			if(clipboard[clipboardLen-1] == '\n'){
+				clipboard[clipboardLen-1] = 0;
+				clipboardLen--;
+			}
+			
 			EraseAllSelectedText(t, &k, c);
 			AddStrToText(t, &k, clipboard);
+			clipboard[clipboardLen] = tmp;
 			t->cursors[k].addedLen = clipboardLen;
 		}
 	}
@@ -2186,7 +2194,9 @@ static void Copy(Thoth_Editor *t, Thoth_EditorCmd *c){
 	}
 	if(buffer){
 		if(t->clipboard) free(t->clipboard);
+		bufferLen++;
 		buffer = realloc(buffer, bufferLen+1);
+		buffer[bufferLen-1] = '\n';
 		buffer[bufferLen] = 0;
 		t->clipboard=buffer;
 #ifdef SDL_COMPILE
